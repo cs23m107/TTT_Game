f36dd81e743640b18cc3cbb379e85c2cc35afba8
cs23m107 <153214424+cs23m107@users.noreply.github.com>
2025-02-11 11:50:14 +0530

    Update Tic-Tac-Toe.cpp

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -11,7 +11,7 @@ void setup_game(){
     }
     return ;
 }
-void display_board(){                                                  // printing board
+void display_board(){                                                  // printing the board
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){ 
             cout<<board[i][j]<<" ";
